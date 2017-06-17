7b925f2f537f1f0b334d61130fcc7070c175f8b7
Yuki Inui <yuuuuki.chi.chi.chi@gmail.com>
2017-06-17 21:21:49 +0900

    test repos for Gitgutter issue #425

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/*
+*	-*- coding: shift_jis -*-
+*
+*	test repos for Gitgutter issue #425
+*	TEST REPOS FOR GITGUTTER ISSUE #425
+*	テスト リポジトリ #425
+*	てすと りぽじとり #425
+*	1234567890 !"#$%&'()=~|"
+*	qwertyuiop@[asdfghjkl;:]zxcvbnm,./\
+*	QWERTYUIOP`{ASDFGHJKL+*}ZXCVBNM<>?_
+*/
+
+int main(int argc, char const *argv[])
+{
+	printf("test repos for Gitgutter issue #425\r\n");
+	printf("TEST REPOS FOR GITGUTTER ISSUE #425\r\n");
+	return 0;
+}
